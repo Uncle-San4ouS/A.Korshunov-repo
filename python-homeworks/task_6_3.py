1aeb19aa6a4bbc9264b7dca94107684036c20b83
A.Korshunov <unclesan4ous@ya.ru>
2021-03-16 22:23:32 +0300

    lesson_6 tasks 6 added

--- python-homeworks/task_6_3.py
+++ python-homeworks/task_6_3.py
@@ -1,15 +1,1 @@
-from json import dump
-from itertools import zip_longest
-
-with open("users.csv", "r", encoding="cp1251") as u:
-    with open("hobby.csv", "r", encoding="cp1251") as h:
-
-        if len(u.readlines()) > len(h.readlines()):
-            with open("user_hobby.txt", "w", encoding="cp1251") as t:
-                u_h = zip_longest(u, h, fillvalue=None)
-                dict = {str(p[0]).strip(): (p[1].strip()) for p in u_h}
-
-                dump(dict, t, ensure_ascii=False, indent=4)
-            print(dict)
-        else:
-            exit(1)
+from
---
+++ python-homeworks/task_6_6.py
@@ -0,0 +1,14 @@
+from sys import argv
+
+with open('bakery.csv', 'a', encoding='utf-8') as cheesecake_a:
+    with open('bakery.csv', 'r', encoding='utf-8') as cheesecake_r:
+        if len(argv) > 1 and [n for n in argv[1:] if n.replace('.', '').isdigit()]:
+            if len(argv) == 3:
+                print(*cheesecake_r.read().split()[int(argv[1]) - 1:int(argv[2])], sep='\n')
+            if ',' in argv[1] or '.' in argv[1]:
+                cheesecake_r.read()
+                print(argv[1], file=cheesecake_a)
+            else:
+                print(*cheesecake_r.readlines()[int(argv[1]) - 1:], sep='')
+        else:
+            print(cheesecake_r.read())
